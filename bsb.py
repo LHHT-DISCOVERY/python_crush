5bf9a59eba34e68ac74fe3d67e36220aecf613d7
your name <lytri102@gmail.com>
2022-04-24 10:58:06 +0700

    update ngày 24/4/2022

---
+++ bsb.py
@@ -0,0 +1,24 @@
+print(" Bạn đang yêu ai ?")
+print("* M \n")
+print("* N \n")
+print("* L \n")
+m = input(" Nhập vào tên Ny bạn : ");
+while(1):
+    if m == "M" :
+        print(" SAI rồi tên ny ban la L  nha")
+        break;
+    if m == "m" :
+        print(" SAI rồi tên ny ban la L  nha")
+        break;
+    if m == "N" :
+        print(" SAI rồi tên ny ban la L  nha")
+        break;
+    if m == "n" :
+        print(" SAI rồi tên ny ban la L  nha")
+        break;
+    if m == "l" or m == "L" :
+        print(" đúng rồi nha")
+        break;
+    else :
+        break ;
+        
